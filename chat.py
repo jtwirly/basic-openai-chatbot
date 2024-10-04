42e0822b1a081a0a896fa5643a3c64c40f66ce8c
Jennifer Turliuk <kingstonlives@gmail.com>
2024-10-04 01:02:06 +0000

    first commit

---
+++ chat.py
@@ -0,0 +1,24 @@
+import os
+from dotenv import load_dotenv
+from openai import OpenAI
+
+load_dotenv()
+
+api_key = os.getenv("OPENAI_API_KEY")
+
+client = OpenAI(api_key=api_key)
+
+user = input("What do you want to ask me?")
+
+completion = client.chat.completions.create(
+    model="gpt-4o-mini",
+    messages=[
+        {"role": "system", "content": "You are a helpful assistant."},
+        {
+            "role": "user",
+            "content": user
+        }
+    ]
+)
+
+print(completion.choices[0].message.content)
